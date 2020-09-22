4b8a5897b246f9c774d6ed9ebc7b8fe570d7fef6
Aman Agrawal <52666677+geass-4m4n@users.noreply.github.com>
2020-09-22 20:44:22 +0530

    Create 413. Arithmetic Slices.cpp

---
+++ leetcode practice/413. Arithmetic Slices.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& a) {
+        
+        int n=a.size();
+        if(n<3)return 0;int ans=0;
+        
+        vector<int>dp(n,0);
+        
+        for(int i=2;i<n;i++)
+            if(a[i]-a[i-1] == a[i-1]-a[i-2])
+            {
+                dp[i]=dp[i-1]+1;
+                ans+=dp[i];
+            }
+        
+        return ans;
+        
+        
+        
+    }
+};
