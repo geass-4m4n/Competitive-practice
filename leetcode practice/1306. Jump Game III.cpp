6728a7c0354a229db8e568a9bd8d338d907cc4a1
Aman Agrawal <52666677+geass-4m4n@users.noreply.github.com>
2020-09-29 22:29:06 +0530

    Create 1306. Jump Game III.cpp

---
+++ leetcode practice/1306. Jump Game III.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        
+         if (start >= 0 && start < arr.size() && arr[start] >= 0)
+         {
+            if (arr[start] == 0) {
+                return true;
+            }
+            arr[start] = -arr[start];
+            return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
+        }
+        
+        return false;
+        
+    }
+};
