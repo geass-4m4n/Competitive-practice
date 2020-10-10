e5540406b690df245c7ddc76fea6c35fc5f38be5
Aman Agrawal <52666677+geass-4m4n@users.noreply.github.com>
2020-10-10 23:36:31 +0530

    Create 611. Valid Triangle Number.cpp

---
+++ leetcode practice/611. Valid Triangle Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int k = i + 1;
+            for (int j = i + 2; j < n; ++j) {
+                while(k < j && nums[i] + nums[k] <= nums[j]) {
+                    ++k;
+                }
+                ans += j - k;
+            }
+        }
+        return ans;
+        
+    }
+};
