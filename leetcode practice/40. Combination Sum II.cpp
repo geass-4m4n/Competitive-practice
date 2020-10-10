e4d29d210684d1c3082b3d9a7420c80bb0d4b045
Aman Agrawal <52666677+geass-4m4n@users.noreply.github.com>
2020-10-11 00:20:27 +0530

    Update 40. Combination Sum II.cpp

--- leetcode practice/40. Combination Sum II.cpp
+++ leetcode practice/40. Combination Sum II.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     
-    void solve(vector<int>& arr, int t,int s,vector<int>r, set<vector<int> >&ans,int id)
+    void solve(vector<int>& arr, int t,int s,vector<int>r, vector<vector<int> >&ans,int id)
     {
         if(s>t)
             return;
         if(s==t)
         {
-            ans.insert(r);
+            ans.push_back(r);
             return;
         }
         if(id>=arr.size())
             return;
         for(int i=id;i<arr.size();i++)
         {
+            if(i>id && arr[i]==arr[i-1])
+                continue;
+            if(arr[i]>t-s)
+                continue;
+            
             r.push_back(arr[i]);
         
             solve(arr,t,s+arr[i],r,ans,i+1);
@@ -23,13 +28,12 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& arr, int t) {
         
         vector<int>r;
-        set<vector<int> >ans;
+        vector<vector<int> >ans;
         sort(arr.begin(),arr.end());
         solve(arr,t,0,r,ans,0);
         
-        vector<vector<int> >res(ans.begin(),ans.end());
         
-        return res;
+        return ans;
         
     }
 };
